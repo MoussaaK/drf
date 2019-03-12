88c5103857ea5e449ee3edca163c11d021fb1853
KONATE Moussa TGI/OLN <DMNR6478@orange.com>
2019-03-12 17:25:41 +0100

    first commit

---
+++ src/restapis/drf/user_group_api/webapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .serializers import GroupSerializer, UserSerializer
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+
+
+# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
